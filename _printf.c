d3733771759650e7624a489169fc1db7fcd0a250
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-06 16:25:55 +0200

    The printf function.

---
+++ _printf.c
@@ -0,0 +1,70 @@
+
+#include "main.h"
+
+void print_buffer(char buffer[], int *buffer_index);
+
+/**
+ * _printf - The printf function
+ * @format: Format specifier
+ * Return: Printed characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int printed = 0, printed_chars = 0, buffer_index = 0;
+	int i, flags, width, precision, size;
+
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buffer_index++] = format[i];
+			if (buffer_index == BUFF_SIZE)
+				print_buffer(buffer, &buffer_index);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buffer_index);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buffer_index);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of characters
+ * @buffer_index: Index at which to add next char, represents the length
+ */
+
+void print_buffer(char buffer[], int *buffer_index)
+{
+	if (*buffer_index > 0)
+		write(1, &buffer[0], *buffer_index);
+
+	*buffer_index = 0;
+}
